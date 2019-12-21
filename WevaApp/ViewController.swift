81629cb0a03a35d5730593a3d93b6e46e46faf9b
Daniel Richards <Kordifier@Gmail.com>
2019-12-21 16:17:00 +0000

    Initial Commit

---
+++ WevaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WevaApp
+//
+//  Created by Daniel Richards on 21/12/2019.
+//  Copyright © 2019 Daniel Richards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
